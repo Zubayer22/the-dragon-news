eb9ee4aaf0ecc69a95b9b338e4cc818f7f27ee44
zubayer22 <zubayerkh@gmail.com>
2023-04-30 01:56:03 +0600

    some issue fixed

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -2,7 +2,8 @@ import moment from 'moment';
 import React from 'react';
 import { Button, Card, Image } from 'react-bootstrap';
 import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
-import Rating from 'react-rating';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({ news }) => {
@@ -29,14 +30,8 @@ const NewsCard = ({ news }) => {
 
                 </Card.Body>
                 <Card.Footer className="text-muted d-flex">
-                    <div className='flex-grow-1'>
-                        <Rating
-                            placeholderRating={rating?.number}
-                            readonly
-                            emptySymbol={<FaRegStar className='text-warning'></FaRegStar>}
-                            placeholderSymbol={<FaStar className='text-warning'></FaStar>}
-                            fullSymbol={<FaStar></FaStar>}
-                        />
+                    <div className='flex-grow-1 d-flex align-items-center'>
+                        <Rating style={{ maxWidth: 100 }} value={Math.round(rating?.number || 0)} readOnly />
                         <span>{rating?.number}</span>
                     </div>
                     <div>
--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const LeftNav = () => {
     const [categories, setCategories] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:3000/categories')
+        fetch('https://the-news-dragon-server-zubayer22.vercel.app/categories')
             .then(res => res.json())
             .then(data => setCategories(data))
             .catch(error => console.error(error))
